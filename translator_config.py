e8c79f2983af469c255b71a5cb8d549457e5767d
Veena-Suraj <164667844+Veena-Suraj@users.noreply.github.com>
2025-08-16 00:11:39 +1000

    Update translator_config.py

--- translator_config.py
+++ translator_config.py
@@ -1,43 +1,53 @@
 SENTENCE = {
     "sent1": {
         "en": "JustAccess 🤖",
-        "sp": "Acceso Justo 🤖"
+        "sp": "Acceso Justo 🤖",
+        "hi": "जस्टएक्सेस 🤖"
     },
     "sent2": {
         "en": "AI Legal Aid Navigator for Underserved Communities",
-        "sp": "Navegador de Asistencia Legal por IA para Comunidades Desatendidas"
+        "sp": "Navegador de Asistencia Legal por IA para Comunidades Desatendidas",
+        "hi": "वंचित समुदायों के लिए एआई कानूनी सहायता नेविगेटर"
     },
     "sent3": {
         "en": "JustAccess is an AI-powered legal aid navigator that can help you with a variety of tasks.",
-        "sp": "JustAccess es un navegador de asistencia legal impulsado por IA que puede ayudarte con una variedad de tareas."
+        "sp": "JustAccess es un navegador de asistencia legal impulsado por IA que puede ayudarte con una variedad de tareas.",
+        "hi": "जस्टएक्सेस एक एआई-संचालित कानूनी सहायता नेविगेटर है जो आपको विभिन्न कार्यों में मदद कर सकता है।"
     },
     "sent4": {
         "en": "Log in with Google",
-        "sp": "Iniciar sesión con Google"
+        "sp": "Iniciar sesión con Google",
+        "hi": "गूगल से लॉग इन करें"
     },
     "sent5": {
         "en": "Log out",
-        "sp": "Cerrar sesión"
+        "sp": "Cerrar sesión",
+        "hi": "लॉग आउट"
     },
     "sent6": {
         "en": "Please Select a Language of Your Choice...",
-        "sp": "Por favor, seleccione un idioma..."
+        "sp": "Por favor, seleccione un idioma...",
+        "hi": "कृपया अपनी पसंद की भाषा चुनें..."
     },
     "sent7": {
         "en": "Select Language",
-        "sp": "Seleccione un idioma"
+        "sp": "Seleccione un idioma",
+        "hi": "भाषा चुने"
     },
     "sent8": {
         "en": "Hello there!!!!, I am a highly trained Legal Aid Navigator. You can ask me any queries about the child care.",
-        "sp": "¡Hola! Soy una asesora legal altamente capacitada. Pueden hacerme cualquier pregunta sobre el cuidado infantil."
+        "sp": "¡Hola! Soy una asesora legal altamente capacitada. Pueden hacerme cualquier pregunta sobre el cuidado infantil.",
+        "hi": "नमस्ते!!!!, मैं एक उच्च प्रशिक्षित कानूनी सहायता नेविगेटर हूँ। आप मुझसे बाल देखभाल से संबंधित कोई भी प्रश्न पूछ सकते हैं।"
     },
     "sent9": {
         "en": "Please login to access this page",
-        "sp": "Por favor, inicie sesión para acceder a esta página"
+        "sp": "Por favor, inicie sesión para acceder a esta página",
+        "hi": "इस पृष्ठ का प्रयोग करने केलिए लॉगिन करें"
     },
     "sent10": {
         "en": "JustAccess ChatBot ⚖️",
-        "sp": "Chatbot de JustAccess ⚖️"
+        "sp": "Chatbot de JustAccess ⚖️",
+        "hi": "जस्टएक्सेस चैटबॉट ⚖️"
     }
 
 }
