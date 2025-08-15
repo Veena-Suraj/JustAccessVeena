6381b6e886c3452e38c35d3fcaca45853e4cc3ec
Veena-Suraj <164667844+Veena-Suraj@users.noreply.github.com>
2025-08-15 23:59:08 +1000

    Update LogIn.py

--- LogIn.py
+++ LogIn.py
@@ -11,7 +11,7 @@ if 'lang' not in st.session_state:
 with st.sidebar:
     option = st.selectbox(
             SENTENCE["sent6"][st.session_state.lang],
-            ("English", "Spanish"),
+            ("English", "Spanish", "Hindi"),
             placeholder=SENTENCE["sent7"][st.session_state.lang],
             index = None
         )
@@ -21,6 +21,9 @@ with st.sidebar:
         st.rerun()
     elif option == "Spanish":
         st.session_state.lang = "sp"
+        st.rerun()
+    elif option == "Hindi":
+        st.session_state.lang = "hi"
         st.rerun()
 
 # Home page
