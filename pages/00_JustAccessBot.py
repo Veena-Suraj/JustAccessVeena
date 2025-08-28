77737c78048ee3d0fe0470e5378635b0fb32098b
Veena-Suraj <164667844+Veena-Suraj@users.noreply.github.com>
2025-08-28 22:51:31 +1000

    Update 00_JustAccessBot.py

--- pages/00_JustAccessBot.py
+++ pages/00_JustAccessBot.py
@@ -50,7 +50,7 @@ st.title(SENTENCE["sent10"][st.session_state.lang])
 if "messages" not in st.session_state or not st.session_state.messages:
     st.session_state.messages = [FIRST_MESSAGE]
 
-st.markdown(SENTENCE[sent11][st.session_state.lang])
+st.markdown(SENTENCE["sent11"][st.session_state.lang])
 
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
